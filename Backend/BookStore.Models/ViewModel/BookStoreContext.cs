b65dd3c5c0da82b8862476540cb2705b3d41be00
Kanani-sheey <109033379+Kanani-sheey@users.noreply.github.com>
2022-07-10 21:18:34 +0530

    Add files via upload

---
+++ Backend/BookStore.Models/ViewModel/BookStoreContext.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace BookStore.Models.ViewModel
+{
+    public partial class BookStoreContext : DbContext
+    {
+        public BookStoreContext()
+        {
+        }
+
+        public BookStoreContext(DbContextOptions<BookStoreContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseNpgsql("Server=localhost;Database=BookStore;User Id=postgres;Password=shrey123");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "English_United States.1252");
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
